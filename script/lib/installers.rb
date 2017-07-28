e86aa87734169b13de30647ccc2e3b9ad5641e3f
Guido Marucci Blas <guidomb@gmail.com>
2017-07-28 11:11:39 -0300

    Better output formatting

--- script/lib/installers.rb
+++ script/lib/installers.rb
@@ -3,8 +3,8 @@ def pip_install(egg_name)
   puts ""
   puts " → Installing '#{egg_name}' using 'pip' ..."
   safe_exec("pip", "install proselint", "-V") do
-    puts "Command 'pip' is not available"
-    puts "Installing 'pip' using 'easy_install' ..."
+    puts "\t * Command 'pip' is not available"
+    puts "\t * Installing 'pip' using 'easy_install' ..."
     pretty_print(`sudo easy_install pip`, "easy_install: ")
   end
 end
@@ -13,15 +13,15 @@ def bundle_install
   puts ""
   puts " → Installing gems using bundler ..."
   safe_exec("bundle", "install") do
-    puts "Command 'bundle' is not available"
-    puts "Installing 'bundle' using 'gem install' ..."
+    puts "\t * Command 'bundle' is not available"
+    puts "\t * Installing 'bundle' using 'gem install' ..."
     pretty_print(`gem install bundler`, "gem: ")
   end
 end
 
 def safe_exec(command, command_args, command_check_args = "-v")
   execute_command = ->() { pretty_print(`#{command} #{command_args}`, "#{command}: ") }
-  if system("#{command} #{command_check_args} 2>&1 1> /dev/null")
+  if system("#{command} #{command_check_args} > /dev/null 2>&1")
     execute_command.call()
   elsif block_given?
     yield
